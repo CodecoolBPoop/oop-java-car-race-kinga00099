896c92d55f087458a52e6fcfbdc6b3cd0077136d
kinga00099 <kinga00099@gmail.com>
2018-10-23 17:33:06 +0200

    Car class implemented

--- src/com/codecool/Car.java
+++ src/com/codecool/Car.java
@@ -1,4 +1,24 @@
 package com.codecool;
 
+import java.util.Random;
+
 class Car extends Vehicle {
+    private static final String[] carNames = {
+            "Tracer",
+            "Cosmos",
+            "Guardian",
+            "Lioness",
+            "Daydream",
+            "Dynamo",
+            "Hollo",
+            "Fire",
+            "Millenium",
+            "Dynamo"
+    };
+
+    Car() {
+        Random random = new Random();
+        setSpeed(random.nextInt(111 - 80) + 80);
+        setName(carNames[random.nextInt(8)] + " " + carNames[random.nextInt(8)]);
+    }
 }
