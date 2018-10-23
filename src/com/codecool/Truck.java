01277c3e71493fe121b7234b558ab228b0314ce0
kinga00099 <kinga00099@gmail.com>
2018-10-23 17:57:34 +0200

    Truck class implemented

--- src/com/codecool/Truck.java
+++ src/com/codecool/Truck.java
@@ -1,4 +1,31 @@
 package com.codecool;
 
+import java.util.Random;
+
 class Truck extends Vehicle {
+    private int normalSpeed = 100;
+    private int breakdownTurnsLeft = 0;
+
+    int getBreakdownTurnsLeft() {
+        return breakdownTurnsLeft;
+    }
+
+    void setBreakdownTurnsLeft(int breakdownTurnsLeft) {
+        this.breakdownTurnsLeft = breakdownTurnsLeft;
+    }
+
+
+    void breakdownRoll() {
+        Random random = new Random();
+        if (random.nextInt(100) + 1 <= 5) {
+            setBreakdownTurnsLeft(2);
+            setMaximumSpeed(0);
+        }
+    }
+
+    Truck() {
+        setSpeed(normalSpeed);
+        Random random = new Random();
+        setName(String.valueOf(random.nextInt(1001)));
+    }
 }
